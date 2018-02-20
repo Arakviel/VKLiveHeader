e7a0ca39b53ff3dfcd9ed48489af794aa122d213
Arakviel <insider.arakviel@gmail.com>
2018-02-20 22:22:34 +0200

    Перенаправление в web

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '4HkiMN0Z-k1jweBST-Cg_lboivKMICRG',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
